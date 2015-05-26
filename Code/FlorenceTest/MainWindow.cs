95b3d1659881fc459a1bdfa20ae09b352d64da8b
onkeliroh <onkeliroh@gmx.de>
2015-05-26 18:05:08 +0200

    2 Oxyplot Tests (running)

---
+++ Code/FlorenceTest/MainWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Florence;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Florence.GtkSharp;
+using Florence;
+
+using Gtk;
+using System.Runtime.InteropServices;
+
+public partial class MainWindow: Gtk.Window
+{
+	private Florence.GtkSharp.PlotWidget FlorenceWidget;
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+
+		InitializeComponents ();
+	}
+
+	private void InitializeComponents()
+	{	
+		FlorenceWidget = new Florence.GtkSharp.PlotWidget ();
+
+		framePlot.Add (FlorenceWidget);
+
+		FlorenceWidget.ButtonPressEvent += (o, e) => Console.WriteLine ("Click");
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnBtnGOClicked (object sender, EventArgs e)
+	{
+	}
+}
---
+++ Code/OxyPlotTest2/MainWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Gtk;
+
+using OxyPlot;
+using OxyPlot.GtkSharp;
+using System.ComponentModel;
+using OxyPlot.Series;
+using GLib;
+
+public partial class MainWindow: Gtk.Window
+{
+	private OxyPlot.GtkSharp.PlotView plotView;
+
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+
+		InitComponents ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnBtnExitClicked (object sender, EventArgs e)
+	{
+		Application.Quit ();
+	}
+
+	private void InitComponents()
+	{
+		var plotModel = new PlotModel {Title="TestPlot",Subtitle="",PlotType=PlotType.Cartesian,Background=OxyColors.White};
+
+		plotModel.Series.Add (new FunctionSeries (Funktion, 0, 1, .1, "a+b"));
+
+		plotView = new PlotView { Model = plotModel };
+		plotView.SetSizeRequest (400, 400);
+		plotView.Visible = true;
+
+		vboxMain.PackStart (plotView);
+
+		this.SetSizeRequest (600, 600);
+		this.ShowAll ();
+	}
+
+	public System.Func<double,double> Funktion = (a) => a + 1;
+}
