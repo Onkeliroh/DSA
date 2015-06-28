f2bcf07ead033f134261600ef88f1128f67e7205
onkeliroh <onkeliroh@gmx.de>
2015-06-28 23:09:35 +0200

    dyn code

--- Code/DynamicCodeTest/Program.cs
+++ Code/DynamicCodeTest/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using Microsoft.CSharp;
 using System.Reflection;
 using System.IO;
+using System.Net;
+using System.Linq;
 
 public class NotDynamicClass
 {
@@ -25,32 +27,41 @@ public class NotDynamicClass
 
 class Program
 {
-	public const int Aint = 42;
-	public const int Bint = 3;
+	public static Dictionary<string,double> parameter_= new Dictionary<string,double>();
 
 	public static void Main ()
 	{
+		EnterParameter ();
+
+		Console.WriteLine ("\n---------------------------------------\n");
+
 		Console.WriteLine ("Please enter a mathematical function:");
 		string func = Console.ReadLine ();
 
-		var parts = func.Split (new char[]{ '+', '-', '*', '/', '(', ')' });
+		string tobecompiled = 
+			@"using System; public class DynamicClass	{ public static double Main(double[] parameters) { return (  function  );}}";
 
-		foreach (string s in parts)
-		{
+
+		int pos = 0;
+		foreach (string s in parameter_.Keys.ToArray()) {
 			Console.WriteLine (s);
+			func=func.Replace(s,"parameters["+pos+"]");
+			pos++;
 		}
 
-		string tobecompiled = 
-			@"using System; public class DynamicClass	{ public static double Main(double a) { return (" + func + @");}}";
 
+//		func = func.Replace ("val2", "parameters[0]");
+//		func = func.Replace("val1","parameters[1]");
+		tobecompiled = tobecompiled.Replace ("function", func);
 
+		Console.WriteLine (tobecompiled);
 
 		var provider = CodeDomProvider.CreateProvider ("c#");
+
 		var options = new CompilerParameters ();
 		var assemblyContainingNotDynamicClass = Path.GetFileName (Assembly.GetExecutingAssembly ().Location);
 		options.ReferencedAssemblies.Add (assemblyContainingNotDynamicClass);
 		var results = provider.CompileAssemblyFromSource (options, new[] { tobecompiled });
-		var resultss = provider.C
 		if (results.Errors.Count > 0)
 		{
 			foreach (var error in results.Errors)
@@ -60,11 +71,25 @@ class Program
 		} else
 		{
 			var t = results.CompiledAssembly.GetType ("DynamicClass");
-			Func<double,double> dings = (Func<double,double>)Delegate.CreateDelegate (typeof(Func<double,double>), t.GetMethod ("Main"));
-			Console.WriteLine (dings (Aint));
-//			double result = (double)t.GetMethod ("Main").Invoke (null, null);
-//			Console.WriteLine (result);
+			Func<double[],double> dings = (Func<double[],double>)Delegate.CreateDelegate (typeof(Func<double[],double>), t.GetMethod ("Main"));
+//			Console.WriteLine (dings (new double[]{Aint,Bint}));
+			Console.WriteLine(dings(parameter_.Values.ToArray<double>()));
 
+		}
+	}
+
+	public static void EnterParameter()
+	{
+		Console.WriteLine ("Please enter a parameter name:");
+		string name = Console.ReadLine ();
+		Console.WriteLine ("Please enter a value for this parameter:");
+		double value = Convert.ToDouble(Console.ReadLine ());
+
+		parameter_.Add (name, value);
+
+		Console.WriteLine ("\nDo you wish to enter another value?:");
+		if (Console.Read () == 'y') {
+			EnterParameter ();
 		}
 	}
 }
